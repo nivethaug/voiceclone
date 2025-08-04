cdc6dbfe921f99cf1db281ecbd0045535826a75f
nivethaug <122332160+nivethaug@users.noreply.github.com>
2025-08-04 07:58:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ echo "Mode: $MODE_TO_RUN"
 if [ "$MODE_TO_RUN" = "pod" ]; then
   python3 -u src/rp_handler.py
 else
-  runpod.serverless.start({"handler": handler})
+python3 -c "import runpod; from src.rp_handler import handler; runpod.serverless.start({'handler': handler})"
 fi
